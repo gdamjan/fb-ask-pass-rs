4019aff5da76797656c078a3f199012556e11704
Vladan Popovic <vladanovic@gmail.com>
2019-08-28 09:57:16 +0200

    Make offsets optional, add --image argument
    Make x and y offsets optional. The default values of the offsets put the
    bgrt image in the center of the screen.
    
    Pass a mutable Framebuffer instance to all drawing functions.
    
    Introduce another cli argument, --image to get the path of the image
    displayed on boot. The default value is the bgrt image path. Also, a
    Config struct was added to keep the parameters throughout the
    application.
    
    Run localy with:
    cargo run -- --write pass.txt --image test.bmp

--- src/drawing.rs
+++ src/drawing.rs
@@ -11,8 +11,8 @@ struct Pixel(Coordinate, Color);
 
 pub struct Frame {
     buffer: Vec<u8>,
-    xoffset: u32,
-    yoffset: u32,
+    xoffset: Option<u32>,
+    yoffset: Option<u32>,
     width: u32,
     height: u32,
     bytes_per_pixel: u32,
@@ -37,7 +37,13 @@ struct Rect {
 //}
 
 impl Frame {
-    fn new(xoffset: u32, yoffset: u32, width: u32, height: u32, bytes_per_pixel: u32) -> Self {
+    fn new(
+        xoffset: Option<u32>,
+        yoffset: Option<u32>,
+        width: u32,
+        height: u32,
+        bytes_per_pixel: u32,
+    ) -> Self {
         Self {
             buffer: vec![0u8; (width * height) as usize],
             xoffset,
@@ -49,11 +55,20 @@ impl Frame {
     }
 
     fn draw_image(&mut self, path: &str) {
+        println!("{}", path);
         let img = bmp::open(path).unwrap();
+        let xoffset = self
+            .xoffset
+            .unwrap_or((self.width / self.bytes_per_pixel) / 2 - img.get_width() / 2);
+        let yoffset = self
+            .yoffset
+            .unwrap_or(self.height / 2 - img.get_height() / 2);
+
         for (x, y) in img.coordinates() {
             let px = img.get_pixel(x, y);
-            let idx = (((y + self.yoffset) * self.width)
-                + ((x + self.xoffset) * self.bytes_per_pixel)) as usize;
+            let xb = (x + xoffset) * self.bytes_per_pixel;
+            let yb = (y + yoffset) * self.width;
+            let idx = (xb + yb) as usize;
             self.buffer[idx] = px.b;
             self.buffer[idx + 1] = px.g;
             self.buffer[idx + 2] = px.r;
@@ -62,8 +77,7 @@ impl Frame {
 
     fn draw(&mut self, shape: Shape) {
         for Pixel(Coordinate(x, y), Color(r, g, b)) in shape.pixels {
-            let idx = (((y + self.yoffset) * self.width)
-                + ((x + self.xoffset) * self.bytes_per_pixel)) as usize;
+            let idx = (y * self.width + x) as usize;
             self.buffer[idx] = b;
             self.buffer[idx + 1] = g;
             self.buffer[idx + 2] = r;
@@ -82,35 +96,30 @@ fn read_u32_from_file(fname: &str) -> io::Result<u32> {
         .map_err(|_| io::Error::new(io::ErrorKind::Other, "can't parse number"))
 }
 
-fn start() -> Option<Framebuffer> {
-    // Disable text mode in current tty
-    Framebuffer::set_kd_mode(KdMode::Graphics).unwrap();
-
-    let mut framebuffer = Framebuffer::new("/dev/fb0").unwrap();
-
-    let xoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/xoffset").unwrap_or(10);
-    let yoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/yoffset").unwrap_or(10);
-    let width = framebuffer.fix_screen_info.line_length;
-    let height = framebuffer.var_screen_info.yres;
-    let bytespp = framebuffer.var_screen_info.bits_per_pixel / 8;
+fn start(fb: &mut Framebuffer, image_path: &str) {
+    let xoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/xoffset").ok();
+    let yoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/yoffset").ok();
+    let bytespp = fb.var_screen_info.bits_per_pixel / 8;
+    let width = fb.fix_screen_info.line_length;
+    let height = fb.var_screen_info.yres;
 
     let mut frame = Frame::new(xoffset, yoffset, width, height, bytespp);
-    // frame.draw_image("/sys/firmware/acpi/bgrt/image");
-    frame.draw_image("./test.bmp");
+    frame.draw_image(image_path);
 
-    framebuffer.write_frame(frame.buffer.as_slice());
-    Some(framebuffer)
+    // Disable text mode in current tty
+    Framebuffer::set_kd_mode(KdMode::Graphics).unwrap();
+    fb.write_frame(frame.buffer.as_slice());
 }
 
 fn stop() {
     let _ = Framebuffer::set_kd_mode(KdMode::Text).unwrap();
 }
 
-fn draw_pass_validate(fb: &Option<Framebuffer>) {}
-fn draw_pass_type(fb: &Option<Framebuffer>) {}
-fn draw_pass_clear(fb: &Option<Framebuffer>) {}
-fn draw_pass_success(fb: &Option<Framebuffer>) {}
-fn draw_pass_fail(fb: &Option<Framebuffer>) {}
+fn draw_pass_validate(fb: &mut Framebuffer) {}
+fn draw_pass_type(fb: &mut Framebuffer) {}
+fn draw_pass_clear(fb: &mut Framebuffer) {}
+fn draw_pass_success(fb: &mut Framebuffer) {}
+fn draw_pass_fail(fb: &mut Framebuffer) {}
 
 #[derive(Debug)]
 pub enum Msg {
@@ -121,19 +130,20 @@ pub enum Msg {
     Fail,
 }
 
-pub fn init() -> Result<Sender<Msg>, io::Error> {
+pub fn init(image_path: String) -> Result<Sender<Msg>, io::Error> {
     let (tx, rx) = channel::<Msg>();
-    let fb: Option<Framebuffer> = start();
+    let mut fb = Framebuffer::new("/dev/fb0").unwrap();
 
     thread::spawn(move || loop {
         match rx.recv().unwrap() {
-            Msg::KeyPressed(Key::Enter) => draw_pass_validate(&fb),
-            Msg::KeyPressed(Key::Char(_)) => draw_pass_type(&fb),
-            Msg::KeyPressed(Key::Escape) => draw_pass_clear(&fb),
-            Msg::Start | Msg::KeyPressed(_) => (),
+            Msg::Start => start(&mut fb, &image_path),
+            Msg::KeyPressed(Key::Enter) => draw_pass_validate(&mut fb),
+            Msg::KeyPressed(Key::Char(_)) => draw_pass_type(&mut fb),
+            Msg::KeyPressed(Key::Escape) => draw_pass_clear(&mut fb),
+            Msg::KeyPressed(_) => (),
             Msg::Stop => stop(),
-            Msg::Fail => draw_pass_fail(&fb),
-            Msg::Success => draw_pass_success(&fb),
+            Msg::Fail => draw_pass_fail(&mut fb),
+            Msg::Success => draw_pass_success(&mut fb),
         };
     });
 
--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,43 @@ use std::env;
 use std::io;
 use std::sync::mpsc::Sender;
 
-fn parse_args(args: &[String]) -> Result<Option<String>, &'static str> {
-    match args.len() {
-        3 => {
-            if &args[1] == "--write" {
-                Ok(Some(args[2].to_string()))
-            } else {
-                Err("only allowed 1st argument is --write")
-            }
+struct Config {
+    image_path: Option<String>,
+    pass_path: Option<String>,
+}
+
+fn parse_args(args: &[String]) -> Result<Config, &'static str> {
+    let config: Option<Config> = match args.iter().map(String::as_str).collect::<Vec<&str>>()[..] {
+        [_, "--image", img, "--write", path] | [_, "--write", path, "--image", img] => {
+            Some(Config {
+                image_path: Some(String::from(img)),
+                pass_path: Some(String::from(path)),
+            })
         }
-        1 => Ok(None),
-        _ => Err("only 0 or 2 arguments are allowed"),
-    }
+        [_, "--write", path] => Some(Config {
+            image_path: None,
+            pass_path: Some(String::from(path)),
+        }),
+        [_, "--image", img] => Some(Config {
+            image_path: Some(String::from(img)),
+            pass_path: None,
+        }),
+        [_] => Some(Config {
+            image_path: None,
+            pass_path: None,
+        }),
+        _ => None,
+    };
+
+    config.ok_or("Possible arguments are --write and --image.")
 }
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let write_to = parse_args(&args).unwrap();
+    let config = parse_args(&args).unwrap();
+    let write_to = config.pass_path;
+    let image_path = config.image_path;
+
     let send_to_draw = |sink: Sender<Msg>| {
         let tx = sink.clone();
         move |msg: Msg| {
@@ -34,7 +54,8 @@ fn main() -> io::Result<()> {
             tx.send(msg).unwrap()
         }
     };
-    let draw = send_to_draw(drawing::init()?);
+    let default_image = String::from("/sys/firmware/acpi/bgrt/image");
+    let draw = send_to_draw(drawing::init(image_path.unwrap_or(default_image))?);
     let draw_key_callback = |k: Key| {
         println!("Drawing key {:?}", k);
         draw(Msg::KeyPressed(k))
